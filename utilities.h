acf57b466cab0cced08286f345ce1b03c380d37c
O(N²) <pan.holingo@gmail.com>
2025-01-15 17:18:51 +0100

    Create utilities.h

---
+++ utilities.h
@@ -0,0 +1,36 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+#include <sys/sem.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+
+// Klucze do IPC
+#define SHM_KEY 1234
+#define SEM_KEY 5678
+#define MSG_KEY 9101
+
+// Maksymalna liczba stolików
+#define MAX_TABLES 10
+
+// Struktura reprezentująca stan stolików
+typedef struct {
+    int tables[MAX_TABLES]; // Liczba miejsc przy każdym stoliku
+} TableState;
+
+// Funkcje pomocnicze
+void sem_lock(int sem_id) {
+    struct sembuf sb = {0, -1, 0}; // Operacje P (blokowanie)
+    if (semop(sem_id, &sb, 1) == -1) {
+        perror("Błąd operacji semafora: lock");
+    } 
+}
+
+void sem_unlock(int sem_id) {
+    struct sembuf sb = {0, 1, 0}; // Operacja V (odblokowanie)
+    if (semop(sem_id, &sb, 1) == -1) {
+        perror("Błąd operacji semafora: unlock");
+    }
+}
+
+#endif // UTILITIES_H
