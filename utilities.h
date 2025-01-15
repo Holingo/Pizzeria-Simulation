9abdfbc42e5fb5e1b623f88c33a057ee19c7eb56
O(N²) <pan.holingo@gmail.com>
2025-01-15 19:28:52 +0100

    Update utilities.h

--- utilities.h
+++ utilities.h
@@ -18,19 +18,20 @@ typedef struct {
     int tables[MAX_TABLES]; // Liczba miejsc przy każdym stoliku
 } TableState;
 
-// Inline funkcje do obsługi semaforów
+// Inline functions to handle semaphores
 static inline void sem_lock(int sem_id) {
-    struct sembuf sb = {0, -1, 0}; // Operacja P (blokowanie)
+    struct sembuf sb = {0, -1, 0}; // P operation (lock)
     if (semop(sem_id, &sb, 1) == -1) {
-        perror("Błąd operacji semafora: lock");
+        perror("Semaphore operation error: lock");
     }
 }
 
 static inline void sem_unlock(int sem_id) {
-    struct sembuf sb = {0, 1, 0}; // Operacja V (odblokowanie)
+    struct sembuf sb = {0, 1, 0}; // V operation (unlock)
     if (semop(sem_id, &sb, 1) == -1) {
-        perror("Błąd operacji semafora: unlock");
+        perror("Semaphore operation error: unlock");
     }
 }
+
 
 #endif // UTILITIES_H
